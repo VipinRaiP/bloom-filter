25e6b81e0ca6c0b790b6e5e6f5548d9497b8c1a9
Replit user <>
2023-06-04 12:38:15 +0000

    improve sapce using bitmap

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bloom
 
 go 1.19
 
-require github.com/spaolacci/murmur3 v1.1.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 type BloomFilter struct{
-  filter []bool
+  filter []int8
   size int32
 }
 
@@ -28,35 +28,59 @@ func murmurhash(key string, size int32) uint32{
 
 func NewBloomFilter(size int32) *BloomFilter{
   return &BloomFilter{
-    filter : make([]bool,size),
+    filter : make([]int8,size),
     size : size,
   }
 }
 
 func (b *BloomFilter) Add(key string){
     idx := murmurhash(key,b.size)
-    b.filter[idx] = true
+    aidx := idx/8
+    bidx := idx % 8
+    b.filter[aidx] = b.filter[aidx] | (1<<bidx)
 }
 
 func (b *BloomFilter) Exists(key string) bool{
   idx := murmurhash(key, b.size)
-  return b.filter[idx]
+  aidx := idx/8
+  bidx := idx%8
+  return (b.filter[aidx] & (1<<bidx))>0
 }
 
 func main(){
   dataset := make([]string,1000)
   dataset_exists := make(map[string]bool)
   dataset_notexists := make(map[string]bool)
-  
 
-  
-  bloom := NewBloomFilter(16)
-  keys := []string {"a","b","c"}
-  for _,key := range keys{
-      bloom.Add(key)
+  for i:=1; i<=900; i++{
+    key := uuid.New().String()
+    dataset = append(dataset, key)
+    dataset_exists[key] = true
   }
 
-  for _,key := range keys{
-    fmt.Println(key, bloom.Exists(key))
+  for i:=1; i<=200; i++{
+    key := uuid.New().String()
+    dataset = append(dataset, key)
+    dataset_notexists[key] = true
+  }
+
+
+  for i:=100;i<=10000;i+=100{
+    bloom := NewBloomFilter(int32(i))
+    for key,_ := range dataset_exists{
+        bloom.Add(key)
+    }
+  
+    false_positive_count := 0
+    
+    for _,key := range dataset{
+      if bloom.Exists(key) && dataset_notexists[key]{
+        false_positive_count++
+      } 
+    }  
+  
+    error_rate := 100 * (float64(false_positive_count)/float64(len(dataset_notexists)))
+    fmt.Println(error_rate)
   }  
+  
 }
